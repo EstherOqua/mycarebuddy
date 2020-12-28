893ffc12657c717ecfb9fefc4e50b83b6a9c3492
EstherOqua <66262441+EstherOqua@users.noreply.github.com>
2020-12-28 16:01:20 +0000

    Focused on backend coding and login/register components

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <Navbar />
+        <Landing />
+      </div>
+      </Router>
+    );
+  }
+}
+export default App;
